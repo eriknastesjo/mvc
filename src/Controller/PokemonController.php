8e27ac5bcc674977f17be23ba64e55244de5b076
eriknastesjo <erna21@student.bth.se>
2022-04-28 17:08:48 +0100

    Added CRU not yet D

--- src/Controller/PokemonController.php
+++ src/Controller/PokemonController.php
@@ -52,21 +52,97 @@ class PokemonController extends AbstractController
         // actually executes the queries (i.e. the INSERT query)
         $entityManager->flush();
 
-        return $this->redirectToRoute('catch');   // change later to show details of added pokemon
+        return $this->redirectToRoute('pokedex');   // change later to show details of added pokemon
     }
 
     /**
-     * @Route("/pokedex/observe", name="observe"), methods={"GET","HEAD"})
+     * @Route("/pokedex/pokedex", name="pokedex"), methods={"GET","HEAD"})
      */
-    public function observePokemon(
-        PokemonRepository $pokemonRepository
-    ): Response {
+    public function pokedex(PokemonRepository $pokemonRepository): Response {
         $allPokemon = $pokemonRepository->findAll();
 
         $data = [
             'allPokemon' => $allPokemon
         ];
 
-        return $this->render('pokemon/observe.html.twig', $data);
+        return $this->render('pokemon/pokedex.html.twig', $data);
     }
+
+    /**
+     * @Route("/pokedex/pokedex/{id}", name="pokedex-single"), methods={"GET","HEAD"})
+     */
+    public function pokedexSingle(
+        PokemonRepository $pokemonRepository,
+        int $id
+    ): Response {
+        $pokemon = $pokemonRepository->find($id);
+
+        $data = [
+            'pokemon' => $pokemon
+        ];
+
+        return $this->render('pokemon/pokedex-single.html.twig', $data);
+    }
+
+    /**
+     * @Route("/pokedex/experiment/{id}", name="experiment"), methods={"GET","HEAD"})
+     */
+    public function experimentPokemon(
+        PokemonRepository $pokemonRepository,
+        int $id
+    ): Response {
+        $pokemon = $pokemonRepository->find($id);
+
+        $data = [
+            'pokemon' => $pokemon
+        ];
+
+        return $this->render('pokemon/experiment.html.twig', $data);
+    }
+
+    /**
+     * @Route("/pokedex/experiment-process", name="experiment-process", methods={"POST"})
+     */
+    public function experimentProcess(
+        ManagerRegistry $doctrine,
+        Request $request
+        ): Response {
+        $entityManager = $doctrine->getManager();
+        $id = $request->request->get('id');
+
+        $pokemon = $entityManager->getRepository(Pokemon::class)->find($id);
+
+        if (!$pokemon) {
+            throw $this->createNotFoundException(
+                'No pokemon found for id ' . $id
+            );
+        }
+
+        $pokemon->setName($request->request->get('name'));
+        $pokemon->setType($request->request->get('type'));
+        $pokemon->setAttack($request->request->get('attack'));
+        $pokemon->setHp($request->request->get('hp'));
+        $pokemon->setDescription($request->request->get('description'));
+
+        $entityManager->flush();
+
+        return $this->redirectToRoute('pokedex');
+    }
+
+    /**
+     * @Route("/pokedex/release/{id}", name="release"), methods={"GET","HEAD"})
+     */
+    public function release(
+        PokemonRepository $pokemonRepository,
+        int $id
+    ): Response {
+        $pokemon = $pokemonRepository->find($id);
+
+        $data = [
+            'pokemon' => $pokemon
+        ];
+
+        return $this->render('pokemon/release.html.twig', $data);
+    }
+
 }
