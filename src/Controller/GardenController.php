7f5ec70e2b084682fe7612327887c65c24d4472a
eriknastesjo <erna21@student.bth.se>
2022-05-09 11:45:01 +0100

    Added home base for project garden

---
+++ src/Controller/GardenController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Card\Player21;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+class GardenController extends AbstractController
+{
+    /**
+     * @Route("/proj", name="garden-home", methods={"GET","HEAD"})
+     */
+    public function gardenHome(): Response
+    {
+        return $this->render('garden/home.html.twig');
+    }
+
+    /**
+     * @Route("/proj/garden", name="garden", methods={"GET","HEAD"})
+     */
+    public function garden(): Response
+    {
+        return $this->render('garden/home.html.twig');
+    }
+
+}
