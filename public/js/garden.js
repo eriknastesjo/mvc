0c15cfac3948ba42552f7d732563a5ec8fa92083
eriknastesjo <erna21@student.bth.se>
2022-05-10 14:07:36 +0100

    Added eventlistender on seeds that changes form actions

--- public/js/garden.js
+++ public/js/garden.js
@@ -3,7 +3,23 @@
 
     // document.body.style.cursor = "wait";
 
+    const forms = document.getElementsByClassName("groundForm");
+    // console.log(forms);
 
+    const seedBox = document.getElementsByClassName("seed");
+    console.log(seedBox);
+
+    for (let i = 0; i < seedBox.length; i++) {
+        seedBox[i].addEventListener("click", function (event) {
+            for (let i = 0; i < forms.length; i++) {
+                forms[i].action = "/proj/add";
+
+                // forms[i].addEventListener("click", function (event) {
+
+                // });
+            }
+        });
+    }
 
 
 })();
