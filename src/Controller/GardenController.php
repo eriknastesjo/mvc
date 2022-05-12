ec3442474b0d9d76518c968bc96b98f5d5e13cee
eriknastesjo <erna21@student.bth.se>
2022-05-12 11:52:03 +0100

    Basic controller and template to render ORM data

--- src/Controller/GardenController.php
+++ src/Controller/GardenController.php
@@ -135,59 +135,12 @@ class GardenController extends AbstractController
     }
 
 
-    // /**
-    //  */
-    // private function addToTablePlant(
-    //     ManagerRegistry $doctrine,
-    //     Plant $plant
-    // ) {
-
-    //     $entityManager = $doctrine->getManager();
-
-    //     $currentDate = new DateTime();
-
-    //     $gardenPlant = new GardenPlant();
-    //     $gardenPlant->setPlant($plant->getName());
-    //     $gardenPlant->setDate($currentDate->format('y-m-d'));
-    //     $gardenPlant->setTime($currentDate->format('H:i'));
-
-    //     // tell Doctrine you want to (eventually) save the plant
-    //     // (no queries yet)
-    //     $entityManager->persist($gardenPlant);
-
-    //     // actually executes the queries (i.e. the INSERT query)
-    //     $entityManager->flush();
-
-    //     return $gardenPlant;      // in method that called -> use $plant->getId() to get Id
-    // }
-
-    // /**
-    //  */
-    // private function addToTableSale(
-    //     ManagerRegistry $doctrine,
-    //     Plant $plant
-    // ) {
-
-    //     $entityManager = $doctrine->getManager();
-
-    //     $currentDate = new DateTime();
-
-    //     $gardenSale = new GardenSales();
-    //     $gardenSale->setId($plant->getId());
-    //     $gardenSale->setPlant($plant->getName());
-    //     $gardenSale->setPrice($plant->getPrice());
-    //     $gardenSale->setDate($currentDate->format('y-m-d'));
-    //     $gardenSale->setTime($currentDate->format('H:i'));
-
-    //     // tell Doctrine you want to (eventually) save the sale
-    //     // (no queries yet)
-    //     $entityManager->persist($gardenSale);
-
-    //     // actually executes the queries (i.e. the INSERT query)
-    //     $entityManager->flush();
-
-    //     // return $this->redirectToRoute('garden');
-    // }
-
+    /**
+     * @Route("/proj/statistics", name="garden-statistics", methods={"GET","HEAD"})
+     */
+    public function gardenStatistics(): Response
+    {
+        return $this->render('garden/statistics.html.twig');
+    }
 
 }
