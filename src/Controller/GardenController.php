db0b9735c7fde2ad0742d7200ff446e1c9308eea
eriknastesjo <erna21@student.bth.se>
2022-05-16 15:31:29 +0100

    Adding more text about code quality

--- src/Controller/GardenController.php
+++ src/Controller/GardenController.php
@@ -6,8 +6,6 @@
 
 namespace App\Controller;
 
-use App\Entity\GardenPlantedSeeds;
-use App\Entity\GardenSales;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
