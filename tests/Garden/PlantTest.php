971361a7aa7a01c205f86efb4b4dcbe665cfd889
eriknastesjo <erna21@student.bth.se>
2022-05-17 04:19:00 +0100

    Adding tests for Plant class

---
+++ tests/Garden/PlantTest.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace App\Garden;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test cases for class Plant.
+ */
+class PlantTest extends TestCase
+{
+    /**
+     * Construct Plant objects with different arguments and verify that the objects are of expected instance.
+     */
+    public function testCreateCardWithDifferentValues()
+    {
+        $plant = new Plant("strawberry", 10, 2, "yummy", 43);
+        $this->assertInstanceOf("\App\Garden\Plant", $plant);
+
+        $plant = new Plant("potato", 55, 6, "sleeping", 2);
+        $this->assertInstanceOf("\App\Garden\Plant", $plant);
+
+        $plant = new Plant("meatball", 100, 99, "metallic", 31);
+        $this->assertInstanceOf("\App\Garden\Plant", $plant);
+    }
+
+    /**
+     * Get id and verify that it is of expected value.
+     */
+    public function testGetId()
+    {
+        $plant = new Plant("strawberry", 10, 2, "yummy", 43);
+        $this->assertEquals(43, $plant->getId());
+
+        $plant = new Plant("strawberry", 10, 2, "yummy", 70);
+        $this->assertEquals(70, $plant->getId());
+    }
+
+    /**
+     * Set id, then get it and verify that it is of expected value.
+     */
+    public function testSetid()
+    {
+        $plant = new Plant("strawberry", 10, 2, "yummy", 43);
+        $plant->setId(42);
+        $this->assertEquals(42, $plant->getId());
+
+        $plant = new Plant("strawberry", 10, 2, "yummy", 70);
+        $plant->setId(100);
+        $this->assertEquals(100, $plant->getId());
+    }
+
+    /**
+     * Get name and verify that it is of expected value.
+     */
+    public function testGetName()
+    {
+        $plant = new Plant("strawberry", 10, 2, "yummy", 43);
+        $this->assertEquals("strawberry", $plant->getName());
+
+        $plant = new Plant("potato", 10, 2, "yummy", 70);
+        $this->assertEquals("potato", $plant->getName());
+    }
+
+    /**
+     * Get image URL and verify that it is of expected value.
+     * Should include growth level in name unless the name is "empty".
+     */
+    public function testGetImageURL()
+    {
+        $plant = new Plant("empty", 10, 2, "yummy", 43);
+        $this->assertEquals("empty", $plant->getImageURL());
+
+        $plant = new Plant("strawberry", 10, 2, "yummy", 43);
+        $this->assertEquals("strawberry2", $plant->getImageURL());
+    }
+
+    /**
+     * Get price and verify that it is of expected value.
+     */
+    public function testGetPrice()
+    {
+        $plant = new Plant("strawberry", 10, 2, "yummy", 43);
+        $this->assertEquals(10, $plant->getPrice());
+
+        $plant = new Plant("strawberry", 22, 2, "yummy", 70);
+        $this->assertEquals(22, $plant->getPrice());
+    }
+
+    /**
+     * Get status and verify that it is of expected value.
+     */
+    public function testGetStatus()
+    {
+        $plant = new Plant("strawberry", 10, 2, "yummy", 43);
+        $this->assertEquals("yummy", $plant->getStatus());
+
+        $plant = new Plant("strawberry", 22, 2, "red", 70);
+        $this->assertEquals("red", $plant->getStatus());
+    }
+
+    /**
+     * Set status, then get it and verify that it is of expected value.
+     */
+    public function testSetStatus()
+    {
+        $plant = new Plant("strawberry", 10, 2, "yummy", 43);
+        $plant->setStatus("disgusting");
+        $this->assertEquals("disgusting", $plant->getStatus());
+
+        $plant = new Plant("strawberry", 22, 2, "red", 70);
+        $plant->setStatus("brown");
+        $this->assertEquals("brown", $plant->getStatus());
+    }
+
+    /**
+     * Get growth level and verify that it is of expected value.
+     */
+    public function testGetGrowthLevel()
+    {
+        $plant = new Plant("strawberry", 10, 2, "yummy", 43);
+        $this->assertEquals(2, $plant->getGrowthLevel());
+
+        $plant = new Plant("strawberry", 22, 99, "red", 70);
+        $this->assertEquals(99, $plant->getGrowthLevel());
+    }
+
+    /**
+     * Increment growth level, then get it and verify that it is of expected value.
+     */
+    public function testSetGrowthLevel()
+    {
+        $plant = new Plant("strawberry", 10, 2, "yummy", 43);
+        $plant->incrementGrowth();
+        $this->assertEquals(2, $plant->getGrowthLevel());
+
+        $plant = new Plant("strawberry", 22, 99, "red", 70);
+        $this->assertEquals(99, $plant->getGrowthLevel());
+    }
+
+}
